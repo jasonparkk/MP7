b0c00bac0d666ca2b6787ea0587c369f0dee19ed
Jason Park <jyp6@illinois.edu>
2018-04-30 20:24:21 -0500

    123123

--- app/src/main/java/edu/illinois/cs/cs125/mp7/Morty.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/Morty.java
@@ -1,12 +1,43 @@
 package edu.illinois.cs.cs125.mp7;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Morty extends MainActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class Morty extends YouTubeBaseActivity {
+
+    YouTubePlayerView mYouTubePlayerView;
+    Button buttonPlay;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.morty_layout);
+        buttonPlay = (Button) findViewById(R.id.mbutton);
+        mYouTubePlayerView = (YouTubePlayerView) findViewById(R.id.morty_quote_one);
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("pk6tONDf3s0");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mYouTubePlayerView.initialize(YouTubeConfig.getApiKey(), mOnInitializedListener);
+            }
+        });
     }
 }
--- app/src/main/java/edu/illinois/cs/cs125/mp7/Rick.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/Rick.java
@@ -1,12 +1,46 @@
 package edu.illinois.cs.cs125.mp7;
 
-import android.os.Bundle;
 
-public class Rick extends MainActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class Rick extends YouTubeBaseActivity {
+
+    YouTubePlayerView r1YouTubePlayerView;
+    Button button1Play;
+    YouTubePlayer.OnInitializedListener r1OnInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rick_layout);
+        button1Play = (Button) findViewById(R.id.rbutton);
+        r1YouTubePlayerView = (YouTubePlayerView) findViewById(R.id.rick_quote_one);
+
+        r1OnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("xw8Ez_XKfZ8");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        button1Play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                r1YouTubePlayerView.initialize(YouTubeConfig.getApiKey(), r1OnInitializedListener);
+            }
+        });
+
+
     }
 }
